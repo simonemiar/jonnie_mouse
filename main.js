d5f8fd3b5fb0b65302382398b519dffebda2d578
simonemiar <79079392+simonemiar@users.noreply.github.com>
2022-05-09 19:04:45 +0200

    click working

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 import './style.css'
 
+const cursor = document.querySelector("#cursor")
+const theBody = document.querySelector("body")
+
+window.addEventListener("mousemove",function(e){
+    cursor.style.left = e.pageX;
+    cursor.style.top = e.pageY;
+
+    theBody.addEventListener("click", function() {
+        console.log("hellow dannie")
+      });
+
+})
